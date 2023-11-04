5d15dc98e320d8a27821ff464076e7070950ec44
Shommon <sdtai2@outlook.com>
2023-11-03 20:22:19 -0400

    Completed 01_helloworld excersise test completed Completed 02_repeatString excersise test completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(text, num) {
+    if (num > 0){
+        let repeat = text;
+        for (i = 0 ;i < num-1; i++){
+            repeat = repeat + text;
+        }
+        return repeat
+    } else if (num < 0){
+        return 'ERROR';
+    } else if (text == ''){
+        return '';
+    } else {
+        return '';
+    }
 };
 
 // Do not edit below this line
