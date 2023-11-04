1110675e6b4e4d9f6ae82338aaa380dd617bc364
Shommon <sdtai2@outlook.com>
2023-11-04 03:22:05 -0400

    Completed 04_removeFromArray exercise with test completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    let mainArray = array;
+    const args = [...arguments].slice(1,arguments.length);
+    for (i in args){
+        if (mainArray.includes(args[i])){
+            mainArray.splice(mainArray.indexOf(args[i]),1);
+        }
+        
+    }
+    return mainArray
 };
 
 // Do not edit below this line
