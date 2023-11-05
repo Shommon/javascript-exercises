15120c27e6ae4f76da2d7d9c15cea37b7f103111
Shommon <sdtai2@outlook.com>
2023-11-05 12:41:24 -0500

    Completed 05_sumAll exercise with tests completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(a,b){
+    let seq = []
+    let sum = (array) => [...array].reduce((a,i)=>a+i,0); 
+    if ([...arguments].every(i => i > 0) && [...arguments].every(i => typeof i != 'string')){
+        if (a < b){
+            seq = sum(Array.from({length:b-a+1}, (_,i) => a + i));
+        } else if (a > b){
+            seq = sum(Array.from({length:a-b+1}, (_,i) => a - i));
+        };
+        return seq;
+    } else {
+        return 'ERROR'
+    }
 };
+// console.log(sumAll(1,4));
 
 // Do not edit below this line
 module.exports = sumAll;
+
+// let array = [1,2,3,4,5,6,7,8];
+// const sum = array.reduce((a,i)=> a + i , 0);
+// console.log(sum);
+
+// let sample = 'string'
+// console.log(typeof sample == 'string')
