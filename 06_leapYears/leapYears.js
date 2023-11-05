c421173f2f5c5c3401c0cc3e41e837b89f59886e
Shommon <sdtai2@outlook.com>
2023-11-05 18:45:20 -0500

    Completed 06_leapYears exercise with tests completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // Check if the loophole leap year is true
+    if (year % 100 == 0 && year % 400 != 0){
+        return false;
+    // Check of year is dividible by 4
+    } else if (year % 4 == 0){
+        return true;
+    } else {
+        return false;
+    }
 };
-
+console.log(leapYears(700));
 // Do not edit below this line
 module.exports = leapYears;
