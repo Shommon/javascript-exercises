f895dcf3ca10a109562f4f2c3cc3900057b4175c
Shommon <sdtai2@outlook.com>
2023-11-06 13:49:49 -0500

    Completed 10_fibonacci exercise with tests completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,25 @@
-const fibonacci = function() {
+let fibStart = [0,1];
 
-};
+//Return Last two items in the array
+function lastTwo(array){
+    return array.slice(array.length-2);;
+}
+
+const fibonacci = function(num) {
+    //convert string to number
+    num = parseInt(num);
+    if (num >= 0){
+        for (i=0; i<num; i++){
+            // Sum last two items and push into the sequence
+            fibStart.push(lastTwo(fibStart).reduce((a,i) => a+i, 0));
+        }
+        return fibStart[num]
+    } else {
+        return "OOPS"
+    }
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+// console.log(fibonacci("8"));
