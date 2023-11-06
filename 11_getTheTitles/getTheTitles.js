08ebfc22efee669116a98c477a37438d39d46b98
Shommon <sdtai2@outlook.com>
2023-11-06 14:15:39 -0500

    Completed 11_getTheTitles exercise with tests completed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,24 @@
-const getTheTitles = function() {
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
 
+
+const getTheTitles = function(collection){
+    const titles = []
+    for (i=0;i< collection.length; i++){
+        titles.push(collection[i].title);
+    }
+    return titles
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
+// console.log(getTheTitles(books));
