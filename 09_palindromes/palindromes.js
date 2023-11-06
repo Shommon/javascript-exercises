4786ea22a963ff71eb84343d922ccac3ccbe644f
Shommon <sdtai2@outlook.com>
2023-11-06 13:11:01 -0500

    Completed 09_palindromes exercise with tests completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
-
+//Remove all Punctuation and white spa
+function processString(string){
+    return string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()?"'<> ]/g, '').toUpperCase();
+}
+function reversed(string){
+    return string.split("").reverse().join("").toUpperCase();
+}
+    
+const palindromes = function (string) {
+    if (processString(reversed(string)) == processString(string)){
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+console.log(palindromes('racecar'))
+// console.log(palindromes('r3ace3car'))
