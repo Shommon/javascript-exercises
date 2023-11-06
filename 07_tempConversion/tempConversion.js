867da3b0ef0af82a0d73f9f75dfdf73dd1faa71e
Shommon <sdtai2@outlook.com>
2023-11-05 21:10:53 -0500

    Completer 07_tempConversion exercise with tests completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celc = (temp-32)*(5/9);
+  if (celc == Math.floor(celc)){
+    return celc;
+  } else {
+    return +celc.toFixed(1);
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let farh = (temp*(9/5))+32;
+  if (farh == Math.floor(farh)){
+    return farh;
+  } else {
+    return +farh.toFixed(1);
+  }
 };
 
 // Do not edit below this line
@@ -9,3 +21,5 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+// console.log(convertToCelsius(100))
